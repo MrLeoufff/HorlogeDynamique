89ca8259e596b7a2c3481762aeedae2f46d81007
mrleoufff <mrleoufff@gmail.com>
2024-10-05 19:15:35 +0200

    régler lle responsive

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ function setTime() {
     dateEl.innerHTML = `${days[day]} <span class="circle">${date}</span> ${months[month]} `
 }
 
+// StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
